7af83016081670e3835226a6fac971dc73d83275
pedro_muhamad <ra193934@ucdb.br>
2023-09-19 10:51:36 -0400

    Visualizacao via bibliotecas

--- atividade-avaliativa-grafos/grafos.py
+++ atividade-avaliativa-grafos/grafos.py
@@ -1,10 +1,13 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def criar_grafo():
-    grafo = {}
+    grafo = nx.Graph()
     while True:
         vertice = input("Digite um vértice (ou 'fim' para parar): ")
         if vertice.lower() == 'fim':
             break
-        grafo[vertice] = []
+        grafo.add_node(vertice)
     
     while True:
         aresta = input("Digite uma aresta no formato 'vértice1-vértice2' (ou 'fim' para parar): ")
@@ -12,21 +15,28 @@ def criar_grafo():
             break
         vertice1, vertice2 = aresta.split('-')
         
-        if vertice1 in grafo and vertice2 in grafo:
-            grafo[vertice1].append(vertice2)
-            grafo[vertice2].append(vertice1)
+        if vertice1 in grafo.nodes() and vertice2 in grafo.nodes():
+            grafo.add_edge(vertice1, vertice2)
         else:
             print("Erro: Vértices não encontrados. Certifique-se de que ambos os vértices existem no grafo.")
 
     return grafo
 
+def visualizar_grafo(grafo):
+    pos = nx.spring_layout(grafo)  # Layout para a visualização
+    nx.draw(grafo, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10, font_color='black')
+    plt.title("Grafo Criado")
+    plt.show()
+
 def main():
-    print("Criação de um Grafo")
+    print("Criação e Visualização de um Grafo")
     grafo = criar_grafo()
     
     print("\nGrafo Criado:")
-    for vertice, vizinhos in grafo.items():
-        print(f"Vértice {vertice}: Arestas para {', '.join(vizinhos)}")
+    for vertice in grafo.nodes():
+        print(f"Vértice {vertice}")
+
+    visualizar_grafo(grafo)
 
 if __name__ == "__main__":
     main()
