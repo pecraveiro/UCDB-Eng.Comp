78c72254e21ea6bd8346466de29524364ee5e138
pedro_muhamad <ra193934@ucdb.br>
2023-09-26 12:10:38 -0400

    salvar-arquivos

--- atividade-avaliativa-grafos/grafos.py
+++ atividade-avaliativa-grafos/grafos.py
@@ -1,27 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-
-def abrir_arquivo_ou_criar_grafo():
-    while True:
-        print("\nOpções:")
-        print("1. Criar um novo grafo")
-        print("2. Abrir um arquivo")
-        print("3. Importar um arquivo")
-        escolha = input("Escolha uma opção: ")
-
-        if escolha == "1":
-            grafo = criar_grafo()
-            return grafo
-        elif escolha == "2":
-            nome_arquivo = input("Digite o nome do arquivo: ")
-            try:
-                grafo = carregar_grafo_de_arquivo(nome_arquivo)
-                return grafo
-            except FileNotFoundError:
-                print(f"Arquivo '{nome_arquivo}' não encontrado. Tente novamente.")
-        else:
-            print("Opção inválida. Tente novamente.")
+import csv
 
 def criar_grafo():
     grafo = nx.Graph()
@@ -44,12 +24,53 @@ def criar_grafo():
 
     return grafo
 
+def abrir_arquivo_ou_criar_grafo():
+    while True:
+        print("\nOpções:")
+        print("1. Criar um novo grafo")
+        print("2. Abrir um arquivo .txt")
+        print("3. Abrir um arquivo .csv")
+        print("0. Sair do menu")
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == "1":
+            grafo = criar_grafo()
+            return grafo
+        elif escolha == "2":
+            nome_arquivo = input("Digite o nome do arquivo TXT: ")
+            try:
+                grafo = carregar_grafo_de_arquivo(nome_arquivo)
+                return grafo
+            except FileNotFoundError:
+                print(f"Arquivo TXT '{nome_arquivo}' não encontrado. Tente novamente. Lembre-se de adicionar a extensão .txt")
+        elif escolha == "3":
+            nome_arquivo_csv = input("Digite o nome do arquivo CSV: ")
+            try:
+                grafo = importar_grafo_de_csv(nome_arquivo_csv)
+                return grafo
+            except FileNotFoundError:
+                print(f"Arquivo CSV '{nome_arquivo_csv}' não encontrado. Tente novamente. Lembre-se de adiconar a extensão .csv")
+        elif escolha == "0":
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
 def carregar_grafo_de_arquivo(nome_arquivo):
     grafo = nx.Graph()
-    with open(nome_arquivo, 'r') as arquivo: #r significa 'read' - será aberto no modo de leitura
+    with open(nome_arquivo, 'r') as arquivo:
         for linha in arquivo:
             vertice1, vertice2 = linha.strip().split('-')
             grafo.add_edge(vertice1, vertice2)
+    return grafo
+
+def importar_grafo_de_csv(nome_arquivo_csv):
+    grafo = nx.Graph()
+    with open(nome_arquivo_csv, 'r') as arquivo:
+        leitor_csv = csv.reader(arquivo)
+        for linha in leitor_csv:
+            if len(linha) == 2:
+                vertice1, vertice2 = linha
+                grafo.add_edge(vertice1, vertice2)
     return grafo
 
 def visualizar_grafo(grafo):
@@ -63,6 +84,11 @@ def calcular_numero_cromatico(grafo):
     num_colors = max(coloring.values()) + 1
     return num_colors
 
+def salvar_grafo_em_arquivo(grafo, nome_arquivo):
+    with open(nome_arquivo, 'w') as arquivo:
+        for aresta in grafo.edges():
+            arquivo.write(f"{aresta[0]}-{aresta[1]}\n")
+
 def propriedades_grafo(grafo):
     while True:
         print("\nOpções de Propriedades do Grafo:")
@@ -75,6 +101,8 @@ def propriedades_grafo(grafo):
         print("7. Árvore do Grafo")
         print("8. Número Cromático do Grafo")
         print("9. Visualizar um novo grafo por arquivo")
+        print("10. Salvar grafo em arquivo .txt")
+        print("11. Salvar grafo em arquivo .csv")
         print("0. Sair")
         
         escolha = input("Escolha uma opção: ")
@@ -109,20 +137,39 @@ def propriedades_grafo(grafo):
         elif escolha == "9":
             nome_arquivo = input("Digite o nome do arquivo e a extensão: ")
             grafo = carregar_grafo_de_arquivo(nome_arquivo)
+        elif escolha == "10":
+            nome_arquivo = input("Digite o nome do arquivo .txt para salvar o grafo: ")
+            salvar_grafo_em_arquivo(grafo, nome_arquivo)
+            print(f"Grafo salvo em {nome_arquivo}.")
+        elif escolha == "11":
+            nome_arquivo = input("Digite o nome do arquivo .csv para salvar o grafo: ")
+            salvar_grafo_em_csv(grafo, nome_arquivo)
+            print(f"Grafo salvo em {nome_arquivo}.")
         elif escolha == "0":
             break
         else:
             print("Opção inválida. Tente novamente.")
 
+def salvar_grafo_em_csv(grafo, nome_arquivo):
+    with open(nome_arquivo, 'w', newline='') as arquivo:
+        escritor_csv = csv.writer(arquivo)
+        for aresta in grafo.edges():
+            escritor_csv.writerow([aresta[0], aresta[1]])
+
 def main():
     print("Criação e Propriedades de um Grafo")
-    grafo = abrir_arquivo_ou_criar_grafo()
-    
-    print("\nGrafo Criado! Selecione as opções no menu:")
-    for vertice, vizinhos in grafo.adjacency():
-        print(f"Vértice {vertice}: Arestas para {', '.join(vizinhos)}")
+    while True:
+        grafo = abrir_arquivo_ou_criar_grafo()
+        
+        if grafo is None:
+            # Usuário escolheu sair do menu
+            break
 
-    propriedades_grafo(grafo)
+        print("\nGrafo Criado! Selecione as opções no menu:")
+        for vertice, vizinhos in grafo.adjacency():
+            print(f"Vértice {vertice}: Arestas para {', '.join(vizinhos)}")
+
+        propriedades_grafo(grafo)
 
 if __name__ == "__main__":
     main()
